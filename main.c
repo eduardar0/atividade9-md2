cead0bd673436dc35bea99a25024252310cfbd0d
Eduarda Rodrigues <eduardarodrigues151108@gmail.com>
2025-05-09 16:39:12 -0300

    add: finalizado

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int saoCoprimos(int a, int b) {
     return algEuclides(a, b) == 1;
 }
 
-// ver se o numero é primo
+
 int primo(int n) {
     if (n <= 1) return 0;
     for (int i = 2; i * i <= n; i++) {
@@ -24,7 +24,7 @@ int primo(int n) {
     return 1;
 }
 
-// inverso modular de G em Z_n usando o Algoritmo Estendido de Euclides
+
 int inversoModular(int G, int n) {
 
     int t = 0, novo_t = 1;
@@ -39,12 +39,12 @@ int inversoModular(int G, int n) {
         novo_r = r - q * novo_r;
         r = temp;
     }
-    if (r > 1) return -1; // não tem inverso
+    if (r > 1) return -1; 
     if (t < 0) t += n;
     return t;
 }
 
-// exponenciação modular eficiente
+
 int exponenciacaoModular(int base, int exp, int mod) {
     int resultado = 1;
     base = base % mod;
@@ -57,7 +57,7 @@ int exponenciacaoModular(int base, int exp, int mod) {
     return resultado;
 }
 
-// Função para calcular a função totiente de Euler φ(n)
+
 int phi(int n) {
     int resultado = n;
     for (int i = 2; i * i <= n; i++) {
@@ -84,7 +84,9 @@ int main() {
 
     printf("\n");
 
-    printf("Etapa 1:Sao coprimos? \n");
+    // Topico 1 do docuemnto: Verificar se G e n são coprimos usando Euclides
+
+    printf("parte 1:Sao coprimos? \n");
     if (algEuclides(G, n) != 1) {
         printf("G = %d e n = %d NAO! Divisao nao possivel.\n", G, n);
         return 1;
@@ -95,9 +97,9 @@ int main() {
         printf("\n");
     }
 
+    // Topico 2 do documeto: Inverso modular de G em Z_n
 
-
-    printf("\nEtapa 2: Inverso modular de %d em Z%d \n",G, n);
+    printf("\nparte 2: Inverso modular de %d em Z%d \n",G, n);
 
     int Ginv = inversoModular(G, n);
 
@@ -105,12 +107,13 @@ int main() {
     printf("Usando os restos encontrados no algoritmo de euclides ao encontar o mdc no teorema de bezoult, o inverso de %d em z%d e: %d\n", G, n, Ginv);
     printf("\n");
 
+    // topico 3 do documento: Divisão modular de H por G
 
-    printf("\nEtapa 3: Divisao modular \n");
+    printf("\nparte 3: Divisao modular \n");
     printf("\n");
     printf("A divisao mudular e caracterizada por usar a multiplicacao modular de H(%d) pelo inverso de G(%d), ou seja, o numero que faca o mod de G por n(%d) resulte em 1;\n", H,G,n );
     printf("\n");
-    printf("O inverso de G ja foi calculado na etapa 2, (resultou em %d);\n", Ginv);
+    printf("O inverso de G ja foi calculado na parte 2, (resultou em %d);\n", Ginv);
     printf("\n");
 
     int a = (H * Ginv) % n;
@@ -121,9 +124,9 @@ int main() {
 
 
 
+     // topico 4 do documento: Verificar se a e n1 são coprimos
 
-
-    printf("\nEtapa 4: a = %d e n1 = %d sao coprimos?? \n", a, n1);
+    printf("\nparte 4: a = %d e n1 = %d sao coprimos?? \n", a, n1);
 
     printf("\n");
 
@@ -139,9 +142,11 @@ int main() {
     }
 
 
-
-    printf("\nEtapa 5: n1(%d) e primo? \n", n1);
+    // topico 5 do documento: Verificar se n1 é primo
+    printf("\nparte 5: n1(%d) e primo? \n", n1);
     int x1;
+    printf("\n");
+    // Topico 6/7 do documento: Caso n1 não seja primo, usar Teorema de Euler
     if (primo
     (n1)) {
         x1 = n1 - 1;
@@ -155,13 +160,28 @@ int main() {
         printf("\n");
     }
 
-    printf("\nEtapa 6: Decompor expoente no formato a = b * q + r \n");
+    // topico 8 do documento: Decompor expoente
+
+    printf("\nparte 6: Decompor expoente no formato a = b * q + r \n");
     int q = x / x1;
     int r = x % x1;
+    printf("\n");
     printf("x = %d = %d * %d + %d\n", x, x1, q, r);
     printf("\n");
 
-    printf("\nEtapa 7: Calcular valores intermediarios \n");
+
+     // topico 9 do documento: Reescrever a expressao a^x mod n1 como:
+    printf("parte 7: Reescrever a expressao a^x mod n1 no formato da congruencia:\n");
+    printf("\n");
+    printf("a^x === (((a^x1)^q mod n1) * (a^r mod n1)) mod n1\n");
+    printf("Ou seja: %d^%d === (((%d^%d)^%d mod %d) * (%d^%d mod %d)) mod %d\n", a, x, a, x1, q, n1, a, r, n1, n1);
+    printf("\n");
+
+
+     // topico 10 do documento: Calcular valores intermediarios
+
+    printf("\nparte 8: Calcular valores intermediarios \n");
+    printf("\n");
     int x2 = exponenciacaoModular(a, x1, n1);
     int x2q = exponenciacaoModular(x2, q, n1);
     int ar = exponenciacaoModular(a, r, n1);
@@ -174,12 +194,14 @@ int main() {
     printf("\n");
 
 
-    printf("\nEtapa 8(final): Resultado final da congruencia \n");
+    //topico 11 do documento: Resultado final
+
+    printf("\nparte 9(final): Resultado final da congruencia \n");
+
+    printf("\n");
     int resultado = (x2q * ar) % n1;
-    printf(" (%d)^%d mod %d = %d\n",  a, x, n1, resultado);
+    printf("(%d)^%d mod %d = %d\n",  a, x, n1, resultado);
     printf("\n");
 
     return 0;
 }
-
-//
